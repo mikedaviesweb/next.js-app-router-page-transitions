620cc1657f4745417ce1b37904aaba494bc164d3
Michael Davies <michaeldavies@Michaels-MacBook-Pro.local>
2023-08-01 09:08:31 +1200

    remove child pages
    Signed-off-by: Michael Davies <michaeldavies@Michaels-MacBook-Pro.local>

--- app/[parent]/template.tsx
+++ app/[parent]/template.tsx
@@ -11,36 +11,8 @@ const Template = ({ children }: { children: ReactNode }) => {
   const parent = segments[0];
 
   return (
-    <div className="overflow-hidden">
-      <div className="p-8 border-4 border-cyan-400 h-screen space-y-8">
-        <div className="p-4 border-4 border-cyan-400 flex gap-x-4">
-          <Link
-            className="flex-1 p-2 rounded bg-cyan-950 flex items-start gap-2 flex-col"
-            href={`/${parent}/child-page-one`}>
-            <pre className="block p-1 border border-white/20 text-mono text-[8px] rounded">
-              {parent}
-            </pre>
-            Child Page One
-          </Link>
-          <Link
-            className="flex-1 p-2 rounded bg-cyan-950 flex items-start gap-2 flex-col"
-            href={`/${parent}/child-page-two`}>
-            <pre className="block p-1 border border-white/20 text-mono text-[8px] rounded">
-              {parent}
-            </pre>
-            Child Page Two
-          </Link>
-          <Link
-            className="flex-1 p-2 rounded bg-cyan-950 flex items-start gap-2 flex-col"
-            href={`/${parent}/child-page-three`}>
-            <pre className="block p-1 border border-white/20 text-mono text-[8px] rounded">
-              {parent}
-            </pre>
-            Child Page Three
-          </Link>
-        </div>
-        <div className="p-8 border-4 border-cyan-400">{children}</div>
-      </div>
+    <div className="overflow-hidden h-screen">
+      <div className="p-8 border-4 border-cyan-400 h-full">{children}</div>
     </div>
   );
 };
